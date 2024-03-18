e716329ba4cb08ba32a5a84a07b3e4c94746226f
santiago206 <160612694+santiago206@users.noreply.github.com>
2024-03-18 08:14:19 -0500

    ciclo while
    ciclo while

---
+++ ciclo while.py
@@ -0,0 +1,34 @@
+
+while(True):
+    print("Menu de Opciones: ")
+
+    print("Personas === 1")
+    print("Vehiculos === 2")
+    print("Universidades === 3")
+    print("Notas === 4")
+    print("Salir === 5")
+
+ 
+    numero = int(input("Escoge una de las Opciones: "))
+  
+    if numero== 1:
+     print( "haz presionado el boton personas")
+        
+    elif numero == 2:
+       print("haz presionado el boton vehiculos")
+    
+    elif numero == 3:
+        print( "haz presionado el boton universidades")
+            
+    elif numero == 4:
+       print( "haz presionado el boton notas")
+
+    elif numero == 5:
+        print("haz finalizado")
+        break
+    else: 
+        print("Seleccione un numero correcto")
+       
+       
+  
+
